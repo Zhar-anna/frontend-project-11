ca83b66f9be3910ee3a3c07bad540ff75fba91f2
Anna Zharova <a.zharova.muctr@gmail.com>
2022-10-03 21:00:24 +0300

    bages, linter, webpack

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Zhar-anna/frontend-project-11/workflows/hexlet-check/badge.svg)](https://github.com/Zhar-anna/frontend-project-11/actions)
+
+### CodeClimate:
+[![Maintainability](https://api.codeclimate.com/v1/badges/5970ee435f3f520c6cc2/maintainability)](https://codeclimate.com/github/Zhar-anna/frontend-project-11/maintainability)
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  devServer: {
+    port: 9000,
+    host: 'localhost',
+    progress: true,
+    compress: true,
+    contentBase: path.join(__dirname, 'dist'),
+  },
+//   output: ,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+        //   loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'template.html',
+    }),
+  ],
+};
