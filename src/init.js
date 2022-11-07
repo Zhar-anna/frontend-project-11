42417f00284191489656ac3a05e6899a3c4fd32a
Anna Zharova <a.zharova.muctr@gmail.com>
2022-11-07 17:57:55 +0300

    step 2 done

--- src/init.js
+++ src/init.js
@@ -1,12 +1,15 @@
-import * as yup from 'yup';
+import { object, string } from 'yup';
 import onChange from 'on-change';
 // import watchedState from './view.js';
 
-const state = {
+
+const init = () => {
+  const state = {
     rssForm: {
-        state: 'valid',
+        state: 'ready',
         errors: {
           exist: 'Rss уже существует',
+          expect: 'Rss успешно добавлены',
           required: 'Не должно быть пустым',
           notUrl: 'Ссылка должна быть валидным URL',
           noRss: 'Ресурс не содержит валидный RSS',
@@ -16,56 +19,44 @@ const state = {
     posts: [],
   };
 // состояние и обработчики
-const init = () => {
+  const rssform = document.querySelector('.rss-form');
+  const input = document.getElementById('url-input');
+  const feedbackElement = document.querySelector('.feedback');
     const watchedState = onChange(state, (path, value) => {
+      console.log(path);
         if (path === 'rssForm.state') {
-            rssform.classList.remove('form-control', 'w-100', 'is-invalid');
+            input.classList.remove('is-invalid');
             feedbackElement.classList.remove('text-success', 'text-danger');
           if (value === 'invalid') {
-            rssform.classList.add('form-control', 'w-100', 'is-invalid');
-            feedback.classList.add('text-danger');
-            feedback.textContent = `${state.rssform.errors.notUrl}`;
-            
+            input.classList.add('form-control', 'w-100', 'is-invalid');
+            feedbackElement.classList.add('text-danger');
+            feedbackElement.textContent = 'Ссылка должна быть валидным URL';
           } else if (value === 'valid') {
-            feedback.classList.add('text-sucess');
+            feedbackElement.classList.add('text-success');
+            feedbackElement.textContent = 'Rss успешно добавлены';
             rssform.reset();
             rssform.focus();
           }
         }
       });
-  const rssform = document.querySelector('.rss-form');
-//   const submit = document.querySelector('.rss-form button[type="submit"]');
-const schema = object({
-    url: yup.string().url().nullable().notOneOf(state.feeds.map(({url}) => url), 'state.rssform.errors.exist')
-})
-  const validate = (url) => {
-    let validUrl;
-  //  На выходе мы получаем либо объект прошедший валидацию, либо будет выброшено исключение (в случае, если были обнаружены ошибки)
-      schema.validate(url)
-      .then((data) => {
-        validUrl = data;
-        return Promise.reject(new Error('Ссылка должна быть валидным URL'));
-      })
-      .catch((error) => {
-        watchedState.value = 'invalid';
-      })
-  //     return {};
-  //   } catch (e) {
-  //     return keyBy(e.inner, 'path');
-  //   }
-  return validUrl;
-  };
-  
+
   rssform.addEventListener('submit', (e) => {
     e.preventDefault();
-    const t = new FormData(e.target).get('url');
-    console.log(validate(t));
-    if (validate(t)) {
-        watchedState.value = 'valid';
-        // в фиды добавить фид state.rssform.feeds.push()
-    } else {
-      watchedState.value = 'invalid';
-    }
+    const schema = object({
+      url: string()
+      .url()
+      .nullable()
+      .notOneOf(state.feeds.map(({ url }) => url), 'state.rssform.errors.exist'),
+  });
+  const url = new FormData(e.target).get('url');
+  schema
+  .validate({ url })
+  .then(() => {
+    watchedState.rssForm.state = 'valid';
+  })
+  .catch(() => {
+    watchedState.rssForm.state = 'invalid';
+  })
   });
 };
 
